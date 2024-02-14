03698628e6071ac15695b046de13c45d86e4fa3c
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-14 22:36:52 +0200

    Change tests org and cover GetOHCL

---
+++ WhatTheCoins.Tests/CoinGecko/GeckoCurrencyServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using WhatTheCoins.API;
+
+namespace WhatTheCoins.Tests;
+
+[TestFixture]
+[TestOf(typeof(GeckoCurrencyService))]
+public class GeckoCurrencyServiceTest
+{
+    [Test]
+    public async Task GetByIdIdeal()
+    {
+        var httpClient = HttpClientMock.MockHttpClient(ExpectedData.GeckoApiGetIdResponse);
+        var service = new GeckoCurrencyService(httpClient);
+        var data = await service.GetByIdAsync("bitcoin");
+        
+        data.Should().BeEquivalentTo(ExpectedData.ExpectedCurrency);
+    }
+
+    [Test]
+    public async Task GetOHCLIdeal()
+    {
+        var httpClient = HttpClientMock.MockHttpClient(ExpectedData.GeckoApiGetOHCLResponse);
+        var service = new GeckoCurrencyService(httpClient);
+        var data = await service.GetOHCL("bitcoin");
+
+        data.Should().BeEquivalentTo(ExpectedData.ExpectedOHCLs);
+    }
+}
