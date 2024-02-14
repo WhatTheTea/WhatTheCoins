7d3d4f6556b825855b07b1b82bd10268c59eb04c
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-14 23:07:30 +0200

    Extract Currency creation to GeckoCurrencyFactory.cs

--- WhatTheCoins.API/CurrencyServices/GeckoCurrencyService.cs
+++ WhatTheCoins.API/CurrencyServices/GeckoCurrencyService.cs
@@ -1,35 +1,16 @@
 ﻿using System.Collections.Immutable;
 using System.Text.Json;
+using WhatTheCoins.API.CurrencyFactories;
 using WhatTheCoins.API.DTO.CoinGecko;
+using WhatTheCoins.API.OHCLFactories;
 
 namespace WhatTheCoins.API;
 
 public class GeckoCurrencyService(HttpClient httpClient) : ICurrencyService
 {
-    private const string CurrencyDataRequest = "https://api.coingecko.com/api/v3/coins/{0}";
     private const string OHCLDataRequest = "https://api.coingecko.com/api/v3/coins/{0}/ohlc?vs_currency={1}&days={2}";
-
-    private static readonly IReadOnlyCollection<string> MarketPlacesURL = new HashSet<string>
-    {
-        "https://www.coingecko.com/en/coins/{0}",
-        "https://coincap.io/assets/{0}"
-    };
-    private static ImmutableArray<string> BuildMarketPlaces(string id)
-    {
-        var marketPlaces = MarketPlacesURL.Select(s => string.Format(s, id))
-            .ToImmutableArray();
-        return marketPlaces;
-    }
-
-    private async Task<CoinGeckoDTO> GetGeckoDTOById(string id)
-    {
-        var response = await httpClient.GetAsync(string.Format(CurrencyDataRequest, id));
-        var rawJson = await response.Content.ReadAsStringAsync();
-        var dto = JsonDocument.Parse(rawJson).Deserialize<CoinGeckoDTO>();
-        return dto ?? throw new InvalidOperationException("JSON deserialized into null");
-    }
-
-
+    private ICandlesFactory _candlesFactory = new GeckoCandlesFactory();
+    private ICurrencyFactory _currencyFactory = new GeckoCurrencyFactory(httpClient);
     private async Task<ImmutableArray<ImmutableArray<double>>> GetOHCLData(string id)
     {
         var response = await httpClient.GetAsync(string.Format(OHCLDataRequest, id, "usd", 7));
@@ -51,14 +32,7 @@ public class GeckoCurrencyService(HttpClient httpClient) : ICurrencyService
 
     public async Task<Currency> GetByIdAsync(string id)
     {
-        var dto = await GetGeckoDTOById(id);
-        var currency = new Currency(dto.Id,
-            dto.Symbol,
-            dto.MarketData.TotalVolume["usd"],
-            dto.MarketData.PriceChange24h ?? 0,
-            dto.MarketData.CurrentPrice.ToImmutableDictionary(),
-            BuildMarketPlaces(dto.Id)
-        );
+        var currency = await _currencyFactory.MakeCurrency(id);
         return currency;
     }
 
