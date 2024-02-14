271038beb41c6e4991429e8d06792b1d288dd9c8
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-14 16:46:17 +0200

    CleanUp DTOs

--- WhatTheCoins.API/DTO/CoinGecko/MarketData.cs
+++ WhatTheCoins.API/DTO/CoinGecko/MarketData.cs
@@ -3,7 +3,7 @@
 namespace WhatTheCoins.API.DTO.CoinGecko;
 
 internal record MarketData(
-    [property: JsonPropertyName("current_price")] CurrentPrice CurrentPrice,
-    [property: JsonPropertyName("total_volume")] TotalVolume TotalVolume,
-    [property: JsonPropertyName("price_change_24h")] double? PriceChange24h
+    [property: JsonPropertyName("current_price")] Dictionary<string, double> CurrentPrice,
+    [property: JsonPropertyName("total_volume")] Dictionary<string, long> TotalVolume,
+    [property: JsonPropertyName("price_change_percentage_24h")] double? PriceChange24h
 );
