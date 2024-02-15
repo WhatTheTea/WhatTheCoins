00088cb9dcc7aaa91b4ca9b31dd817043b1721d9
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-15 04:53:21 +0200

    Extract HttpResponses from ExpectedData

--- WhatTheCoins.Tests/CoinGecko/CoinGeckoApiProvider.cs
+++ WhatTheCoins.Tests/CoinGecko/CoinGeckoApiProvider.cs
@@ -9,7 +9,7 @@ public class CoinGeckoApiProvider
     [Test]
     public async Task GetByIdIdeal()
     {
-        var httpClient = HttpClientMock.MockHttpClient(ExpectedData.GeckoApiGetIdResponse);
+        var httpClient = HttpClientMock.MockHttpClient(HttpResponses.GeckoApiGetIdResponse);
         var service = new CurrencyService(new API.ApiProviders.CoinGeckoApiProvider(httpClient));
         
         var data = await service.GetByIdAsync("bitcoin");
@@ -20,7 +20,7 @@ public class CoinGeckoApiProvider
     [Test]
     public async Task GetCandlesIdeal()
     {
-        var httpClient = HttpClientMock.MockHttpClient(ExpectedData.GeckoApiGetOHCLResponse);
+        var httpClient = HttpClientMock.MockHttpClient(HttpResponses.GeckoApiGetOHCLResponse);
         var service = new CurrencyService(new API.ApiProviders.CoinGeckoApiProvider(httpClient));
         
         var data = await service.GetCandles("bitcoin");
@@ -31,11 +31,18 @@ public class CoinGeckoApiProvider
     [Test]
     public async Task SearchByCode()
     {
-        var httpClient = HttpClientMock.MockHttpClient(ExpectedData.GeckoApiSearchResponseBTC);
+        var httpClient = HttpClientMock.MockHttpClient(HttpResponses.GeckoApiSearchResponseBTC);
         var service = new CurrencyService(new API.ApiProviders.CoinGeckoApiProvider(httpClient));
         
         var data = await service.SearchAsync("btc");
 
         data.Should().BeEquivalentTo(ExpectedData.ExpectedSearchResultBTC);
+    }
+
+    [Test]
+    public async Task Top10()
+    {
+        // var httpClient = HttpClientMock.MockHttpClient();
+        // var service = new CurrencyService(new API.ApiProviders.CoinGeckoApiProvider(httpClient));
     }
 }
