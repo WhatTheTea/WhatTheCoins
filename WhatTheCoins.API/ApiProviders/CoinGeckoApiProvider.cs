f4425e1ee95f17c3015a9e96b7690d463c231845
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-16 17:16:15 +0200

    Fix JSON to DTO conversion

--- WhatTheCoins.API/ApiProviders/CoinGeckoApiProvider.cs
+++ WhatTheCoins.API/ApiProviders/CoinGeckoApiProvider.cs
@@ -5,14 +5,9 @@ namespace WhatTheCoins.API.ApiProviders;
 public class CoinGeckoApiProvider(HttpClient httpClient) : ApiProviderBase(httpClient)
 {
     private const string CurrencyDataRequestURL = "https://api.coingecko.com/api/v3/coins/{0}";
-
-    private const string CandlesDataRequestURL =
-        "https://api.coingecko.com/api/v3/coins/{0}/ohlc?vs_currency={1}&days={2}";
-
+    private const string CandlesDataRequestURL = "https://api.coingecko.com/api/v3/coins/{0}/ohlc?vs_currency={1}&days={2}";
     private const string SearchRequestURL = "https://api.coingecko.com/api/v3/search?query={0}";
-
-    private const string Top10RequestURL =
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&locale=en";
+    private const string Top10RequestURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&locale=en";
 
     public override async Task<Currency> GetByIdAsync(string id)
     {
@@ -21,22 +16,17 @@ public class CoinGeckoApiProvider(HttpClient httpClient) : ApiProviderBase(httpC
                   throw new Exception("id not found on CoinGecko");
         return dto.ToCurrency();
     }
-
-    
-
     public override async Task<string?> SearchAsync(string query)
     {
         var dto = await GetDTO<DTO.CoinGecko.Search.DTO>(string.Format(SearchRequestURL, query));
         return dto?.Coins[0].Id;
     }
-
     public override async Task<IImmutableList<Currency>> GetTop10Async()
     {
         var rawCurrencies = await GetDTO<ImmutableArray<DTO.CoinGecko.Currency.DTO>>(Top10RequestURL);
         var currencies = rawCurrencies.Select(dto => dto.ToCurrency()).ToImmutableArray();
         return currencies;
     }
-
     public override async Task<IImmutableList<Candle>> GetCandles(string id, int days = 7,
         string referenceCurrency = "usd")
     {
@@ -51,7 +41,6 @@ public class CoinGeckoApiProvider(HttpClient httpClient) : ApiProviderBase(httpC
                 arr[1], arr[2], arr[3], arr[4])
         ).ToImmutableArray();
     }
-
     private static DateTime ConvertUnixToDateTime(long ms)
     {
         return DateTimeOffset.FromUnixTimeMilliseconds(ms).DateTime;
--- WhatTheCoins.API/DTO/CoinGecko/Currency/DTO.cs
+++ WhatTheCoins.API/DTO/CoinGecko/Currency/DTO.cs
@@ -3,37 +3,26 @@ using System.Text.Json.Serialization;
 
 namespace WhatTheCoins.API.DTO.CoinGecko.Currency;
 
-internal record DTO : CurrencyDTO
-{
-    internal DTO(string id, string symbol, string name, Localization localization, Description description, Links links, Image image, int marketCapRank, MarketData marketData)
-    {
-        Id = id;
-        Symbol = symbol;
-        Name = name;
-        Localization = localization;
-        Description = description;
-        Links = links;
-        Image = image;
-        MarketCapRank = marketCapRank;
-        MarketData = marketData;
-    }
-
-    [property: JsonPropertyName("id")] string Id{ get; init; }
-    [property: JsonPropertyName("symbol")] string Symbol{ get; init; }
-    [property: JsonPropertyName("name")] string Name{ get; init; }
+internal record DTO(
+    [property: JsonPropertyName("id")] string Id,
+    [property: JsonPropertyName("symbol")] string Symbol,
+    [property: JsonPropertyName("name")] string Name,
     [property: JsonPropertyName("localization")]
-    Localization Localization{ get; init; }
+    Localization Localization,
     [property: JsonPropertyName("description")]
-    Description Description{ get; init; }
-    [property: JsonPropertyName("links")] Links Links{ get; init; }
-    [property: JsonPropertyName("image")] Image Image{ get; init; }
+    Description Description,
+    [property: JsonPropertyName("links")] Links Links,
+    [property: JsonPropertyName("image")] Image Image,
     [property: JsonPropertyName("market_cap_rank")]
-    int MarketCapRank{ get; init; }
+    int MarketCapRank,
     [property: JsonPropertyName("market_data")]
-    private MarketData MarketData { get; init; }
-
-    internal override API.Currency ToCurrency() => 
-    new API.Currency(Id, Symbol, MarketData.TotalVolume["usd"],
-                MarketData.PriceChange24h ?? 0, MarketData.CurrentPrice.ToImmutableDictionary(), 
-                BuildMarketPlaces(Id));
+    MarketData MarketData
+) : CurrencyDTO
+{
+    internal override API.Currency ToCurrency()
+    {
+        return new API.Currency(Id, Symbol, MarketData.TotalVolume["usd"],
+            MarketData.PriceChange24h ?? 0, MarketData.CurrentPrice.ToImmutableDictionary(),
+            BuildMarketPlaces(Id));
+    }
 }
