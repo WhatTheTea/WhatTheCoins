7019254f497f75505d0b97593a1b9702b29608b4
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-16 21:11:00 +0200

    Add CoinCap DTOs

---
+++ WhatTheCoins.API/DTO/CoinCap/Currency/DTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WhatTheCoins.API.DTO.CoinCap.Currency;
+
+internal record DTO(
+    [property: JsonPropertyName("data")] Data Data,
+    [property: JsonPropertyName("timestamp")]
+    long Timestamp) : CurrencyDTO
+{
+    internal override API.Currency ToCurrency()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WhatTheCoins.API/DTO/CoinCap/Rates/DTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WhatTheCoins.API.DTO.CoinCap.Rates;
+
+internal class DTO(
+    [property: JsonPropertyName("data")] IReadOnlyList<Data> Data,
+    [property: JsonPropertyName("timestamp")]
+    long Timestamp
+);
