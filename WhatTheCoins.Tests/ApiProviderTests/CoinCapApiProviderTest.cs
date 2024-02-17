c53ee013a465891cc76fd47968e836c45939f467
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-17 03:08:49 +0200

    Fix GetById CoinCap testing

--- WhatTheCoins.Tests/ApiProviderTests/CoinCapApiProviderTest.cs
+++ WhatTheCoins.Tests/ApiProviderTests/CoinCapApiProviderTest.cs
@@ -8,7 +8,7 @@ public class CoinCapApiProviderTest<TApiProvider> : ApiProviderTest<TApiProvider
 {
     public override void SetUpFixture()
     {
-        GetIdResponse = "{\"data\":{\"id\":\"bitcoin\",\"rank\":\"1\",\"symbol\":\"BTC\",\"name\":\"Bitcoin\",\"supply\":\"17193925.0000000000000000\",\"maxSupply\":\"21000000.0000000000000000\",\"marketCapUsd\":\"119179791817.6740161068269075\",\"volumeUsd24Hr\":\"33970260124\",\"priceUsd\":\"6931.5058555666618359\",\"changePercent24Hr\":\"-2.72424\",\"vwap24Hr\":\"7175.0663247679233209\"},\"timestamp\":1533581098863}"; 
+        GetIdResponse = "{\"data\":{\"id\":\"bitcoin\",\"rank\":\"1\",\"symbol\":\"BTC\",\"name\":\"Bitcoin\",\"supply\":\"17193925.0000000000000000\",\"maxSupply\":\"21000000.0000000000000000\",\"marketCapUsd\":\"119179791817.6740161068269075\",\"volumeUsd24Hr\":\"33970260124\",\"priceUsd\":\"49418\",\"changePercent24Hr\":\"-2.72424\",\"vwap24Hr\":\"7175.0663247679233209\"},\"timestamp\":1533581098863}"; 
         GetOHCLResponse = "";
         SearchResponse = "";
         Top10Response = "";
