20da74a63ce30e4008ba0f4b3d35e3b7a8a305f2
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-18 18:56:14 +0200

    Add IHttpClientFactory

--- WhatTheCoins.API/ApiProviders/ApiProviderBase.cs
+++ WhatTheCoins.API/ApiProviders/ApiProviderBase.cs
@@ -3,8 +3,9 @@ using System.Text.Json;
 
 namespace WhatTheCoins.API.ApiProviders;
 
-public abstract class ApiProviderBase(HttpClient httpClient) : IApiProvider
+public abstract class ApiProviderBase(IHttpClientFactory httpClientFactory) : IApiProvider
 {
+    private readonly HttpClient _httpClient = httpClientFactory.CreateClient();
     public abstract Task<Currency> GetByIdAsync(string id);
     public abstract Task<IImmutableList<string>> SearchAsync(string query);
     public abstract Task<IImmutableList<string>> GetTop10Async();
@@ -12,7 +13,7 @@ public abstract class ApiProviderBase(HttpClient httpClient) : IApiProvider
 
     protected async Task<T> GetDTO<T>(string requestURL)
     {
-        var request = await httpClient.GetAsync(string.Format(requestURL));
+        var request = await _httpClient.GetAsync(string.Format(requestURL));
         var rawJSON = await request.Content.ReadAsStringAsync();
         if (string.IsNullOrEmpty(requestURL)) throw new Exception("Request url is empty");
         if (string.IsNullOrEmpty(rawJSON)) throw new Exception("Api returned empty string");
