17e0c09d9465fbe391f37b6fa4258f76497b15a6
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-20 02:49:02 +0200

    Add Top10 Page

--- WhatTheCoins.UI/Views/Pages/Top10Page.xaml.cs
+++ WhatTheCoins.UI/Views/Pages/Top10Page.xaml.cs
@@ -1,11 +1,25 @@
-﻿using System.Windows.Controls;
+﻿using System.Reactive.Disposables;
+using System.Windows.Controls;
+using ReactiveUI;
+using Splat;
+using WhatTheCoins.UI.ViewModels;
 
 namespace WhatTheCoins.UI.Views.Pages;
 
-public partial class Top10Page : Page
+public partial class Top10Page
 {
     public Top10Page()
     {
         InitializeComponent();
+        ViewModel = Locator.Current.GetService<TopPageViewModel>()!;
+        
+        this.WhenActivated(disposable =>
+            {
+                this.OneWayBind(ViewModel, 
+                    vm => vm.TopCurrencies, 
+                    page => page.TopListBox.ItemsSource)
+                    .DisposeWith(disposable);
+            }
+        );
     }
 }
